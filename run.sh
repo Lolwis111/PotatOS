9e13bd7b95c5d9e8a266c599f9d37c5c1be625f0
Levin Palm <lolwissoftware@live.de>
2017-10-20 18:08:53 +0200

    translated run.sh to english

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-image_name="potatos.img"
+image_name="potatos.img" # image file
 
-if [ ! -e $image_name ] ; then
-    echo "'$image_name' nicht gefunden!"
-    echo "Wurde das System gebaut?"
+if [ ! -e $image_name ] ; then # check if the image exists
+    echo "'$image_name' not found!"
+    echo "Make sure to run buildTools.sh and build.sh prior to run.sh"
     exit
 fi
 
-if [ -e "/usr/bin/qemu-system-i386" ] ; then
+if [ -e "/usr/bin/qemu-system-i386" ] ; then # check if qemu exists
     qemu-system-i386 -drive format=raw,file=potatos.img,index=0,if=floppy
 elif [ -e "/usr/bin/qemu-system-x86_64" ] ; then
     qemu-system-x86_64 -drive format=raw,file=potatos.img,index=0,if=floppy
 else
-    echo "Qemu für x86 oder x86_64 muss installiert sein."
+    echo "Please install qemu x86/x64 for this to work!"
 fi
