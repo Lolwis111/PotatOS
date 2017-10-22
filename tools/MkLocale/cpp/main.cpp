3dbe77fb4d2a1520088074e95fcd0cbdd73192a7
Levin Palm <lolwissoftware@live.de>
2017-10-22 13:47:28 +0200

    language files are now easier to handle + updated MkLocale tool

--- tools/MkLocale/cpp/main.cpp
+++ tools/MkLocale/cpp/main.cpp
@@ -9,25 +9,25 @@
 #include "../header/language.h"
 #include "../header/Strings.h"
 
-#define MAX_CHAR_COUNT 4096 // die Strings.sys kann aktuell maximal eine Größe von 4096 Bytes haben
+#define MAX_CHAR_COUNT 4096 // strings.sys is limited to 4096 bytes
 
 int main(int argc, char **argv)
 {
-    if(argc < 3) // es müssen zwei Argumente angegeben werden
+    if(argc < 3) // there have to be two arguments
     {
         std::cout << "Not enough arguments given!" << std::endl;
 
         return 0;
     }
 
-    if(argv[1] == NULL) // Argument 1 ist die Sprache
+    if(argv[1] == NULL) // first argument describes the langauge
     {
         std::cout << "Language can't be NULL!" << std::endl;
 
         return 0;
     }
 
-    if(argv[2] == NULL) // Argument 2 ist das Arbeitsverzeichnis (root von PotatOS)
+    if(argv[2] == NULL) // the second one is the current pwd
     {
         std::cout << "PotatOS-Root can't be NULL!" << std::endl;
     }
@@ -35,14 +35,14 @@ int main(int argc, char **argv)
     std::string argument(argv[1]);
     std::string posRoot(argv[2]);
 
-    std::string fileName = posRoot + "/lang/" + argument; // Pfad zur Sprache bauen
+    std::string fileName = posRoot + "/lang/" + argument; // build the path
 
     struct stat buffer;
-    if(stat(fileName.c_str(), &buffer) != 0) // Prüfen ob zur angegebnen Sprache 
-    {                                        // eine Datei exisiert
+    if(stat(fileName.c_str(), &buffer) != 0) // check if the language exists
+    {
 
-        // wenn nein dann abbrechen
-        std::cout << "Language " << fileName << " was not found!" << std::endl;
+        // if no -> its an error
+        std::cerr << "Language " << fileName << " was not found!" << std::endl;
 
         return 0;
     }
@@ -52,56 +52,87 @@ int main(int argc, char **argv)
 
     std::vector<SYS_STRING> strings;
 
-    int charSum = 0;
-    while(std::getline(inFile, line)) // jede Zeile in der Datei entspricht einem String
-    {
-        size_t split = line.find("="); // welcher im Format NAME="WERT" vorliegen muss
-        std::string attr = line.substr(0, split); // Name extrahieren
-        
-        size_t vStart = line.find("\"", split); // Start-
-        size_t vEnd = line.find("\"", vStart+1); //und Endanführungszeichen suchen
+	std::vector<std::string> lines;
 
-        std::string value = line.substr(vStart + 1, vEnd - vStart - 1); // den Wert extrahieren
-         
-        StringReplace(value, "\\n", "\n\r"); // newLines erzeugen
+	while(std::getline(inFile, line)) // read in all the lines of the file
+	{
+		lines.push_back(line);
+	}
 
-        struct SYS_STRING str;     // einen neuen Systemstring mit den gerade
-        str.size = value.length(); // extrahierten Werten erzeugen
-        str.value = value;
-        str.name = attr;
+	int charSum = 0;
 
-        charSum += str.size;    // die Länge in Bytes mitzählen
+	bool inBlock = false; 	// indicates if we're reading a BEGIN...END
+							// block right now
+	std::string currentName("");
+	std::string currentString("\\r\\n");	
+			
+	for(size_t i = 0; i < lines.size(); i++) 
+	{
+		if(!inBlock)
+		{
+			// check if the line starts with BEGIN
+			if(lines[i].compare(0, 6, "BEGIN ") == 0)
+			{
+				inBlock = true; // if yes we found a block
+				currentName = lines[i].substr(6); // 
+				currentString = "\\r\\n";
+			}
+			// else: ignore lines which are not in BEGIN...END blocks
+		}
+		else if(inBlock)
+		{
+			if(lines[i].compare(0, 3, "END") == 0)
+			{
+				inBlock = false;
+				
+				struct SYS_STRING str;
+				str.size = currentString.length();
+				str.value = currentString;
+				str.name = currentName;
+				
+				charSum += str.size;
+				
+				strings.push_back(str);
+			}
+			else
+			{
+				currentString += lines[i];
+				currentString += "\\r\\n";
+			}
+		}
+	}
+	
+	struct SYS_STRING newLine; // hardcoded string: Newline
+	newLine.size = 2;
+	newLine.value = "\\r\\n";
+	newLine.name = "NEWLINE";
 
-        strings.push_back(str); // den neuen SystemString in einer List ablegen
-    }
+	strings.push_back(newLine);
 
     inFile.close();
 
-    if(charSum > MAX_CHAR_COUNT) // Überprüfen ob die Maximalgröße eingehalten wurde
+    if(charSum > MAX_CHAR_COUNT) // check if we reached size limit
     {
-        std::cout << "Length of " 
+        std::cerr << "Length of " 
                   << MAX_CHAR_COUNT << " exceeded by " 
                   << (charSum - MAX_CHAR_COUNT) << " Bytes";
 
         return -1;
     }
 
-    fileName = posRoot + "/include/language.asm"; // den Pfad zur language.asm bauen
-    std::string fileName2 = posRoot + "/misc/strings.sys"; // den Pfad zur strings.sys bauen
+    fileName = posRoot + "/include/language.asm"; // build path to output file
+    std::string fileName2 = posRoot + "/misc/strings.sys"; // build path to second output file
     std::ofstream languageFile(fileName.c_str());
     std::ofstream stringsFile(fileName2.c_str());
 
     /*
-     * In der Datei language.asm werden nur die (absoluten) Adressen
-     * zu den einzelnen Strings abgelegt. Diese Datei kann in jedes
-     * Programm Problemlos eingebunden werden, da es die Größe der
-     * Binärdatei nicht verändert.
+     * The file language.asm only contains the absolut addresses to the
+     * strings. Every program can include this file and use the defines
+     * inside to address the system string.
      *
-     * Die eigentlichen Strings werden alle hintereinander in die Strings.sys
-     * geschrieben (\0-terminiert). 
-     * Diese Datei muss von keinem Programm explizit eingebunden werden, denn
-     * das System legt diese Automatisch in den Arbeitsspeicher.
-     * Wichtig sind nur die Adresse aus language.asm.
+     * The strings itself are stored in strings.sys, which is loaded
+     * by the OS. Software developers don't have to worry about
+     * this file.
      */
 
     languageFile << "%ifndef _LANGUAGE_H_" << std::endl;
@@ -111,11 +142,18 @@ int main(int argc, char **argv)
 
     for(unsigned i = 0; i < strings.size(); i++)
     {
-        languageFile << "  %define " << strings[i].name << " " << offset << std::endl;
+		// define offset for that string in language.asm
+		
+        languageFile << "  %define " << strings[i].name
+					 << " " << offset << std::endl;
+					 
+		// calculate offset of next string
         offset += strings[i].size + 1;
 
+		// write the string itself
         const char *buffer = strings[i].value.c_str();
         stringsFile.write(buffer, strlen(buffer));
+        // and zero terminate it
         stringsFile.put((char)0x00);
     }
 
--- tools/MkLocale/header/Strings.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STRING_MANIPULATE_H_
-#define _STRING_MANIPULATE_H_
-
-#include <string>
-
-// ersetzt alle Vorkommen von <search> durch <replace> in <str>.
-int StringReplace(std::string& str, const std::string& search, const std::string& replace);
-
-#endif
