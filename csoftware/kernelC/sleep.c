7b57b665607571751e8f4fc7b4f5e2cbc5d0cf79
Levin Palm <levin.palm@live.com>
2024-11-20 13:08:10 +0100

    fixed setTimer (wrong command byte)

---
+++ csoftware/kernelC/sleep.c
@@ -0,0 +1,35 @@
+#include "sleep.h"
+#include "io.h"
+#include "pic.h"
+
+static volatile int sleepCounter = 0;
+
+__attribute__((interrupt)) void timer_isr(struct interrupt_frame* frame)
+{
+    if(sleepCounter > 0)
+    {
+        sleepCounter--;
+    }
+
+    PIC_sendEOI(0);
+}
+
+void setTimer(int hz)
+{
+    int divisor = 1193180 / hz;       /* Calculate our divisor */
+    outportb(0x43, 0x34);             /* Set our command byte 0x34 */
+    io_wait();
+    outportb(0x40, divisor & 0xFF);   /* Set low byte of divisor */
+    io_wait();
+    outportb(0x40, divisor >> 8);     /* Set high byte of divisor */
+}
+
+void sleep(int time)
+{
+    sleepCounter = time;
+
+    while (sleepCounter > 0)
+    {
+        __asm__ volatile("hlt;":::);
+    }
+}
